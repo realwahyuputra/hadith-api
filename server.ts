a45ada82e71624b622c2ab12d3c7cb090198f7ea
realwahyuputra <realwahyuputra@gmail.com>
2025-03-13 10:27:13 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,31 +3,38 @@ import Cors from 'cors'
 import Routes from './api/Routes'
 
 class Server {
-  private application: Application
+  public application: Application
   private port: number | string
-
+  
   constructor() {
     this.port = process.env.PORT || 3300
     this.application = Express()
     this.plugins()
     this.routes()
   }
-
+  
   private plugins(): void {
     this.application.use(Express.urlencoded({ extended: true }))
     this.application.use(Express.json())
     this.application.use(Cors())
   }
-
+  
   private routes(): void {
     this.application.use(Routes)
   }
-
+  
   public run(): void {
-    this.application.listen(this.port, () => {
-      console.log(`Server listening on http://localhost:${this.port}`)
-    })
+    if (process.env.NODE_ENV !== 'production') {
+      this.application.listen(this.port, () => {
+        console.log(`Server listening on http://localhost:${this.port}`)
+      })
+    }
   }
 }
 
-export default new Server().run()
+// Create server instance
+const server = new Server();
+// Run the server in non-production environments
+server.run();
+// Export the Express application instance for Vercel
+export default server.application;
