b117e17348a770c6b1f7502d5fa83ad3d4ad38f9
realwahyuputra <realwahyuputra@gmail.com>
2025-03-13 11:50:28 +0700

    Update Hadith.ts

--- api/Routes/Hadith.ts
+++ api/Routes/Hadith.ts
@@ -8,7 +8,9 @@ class HadithRoute extends Router<typeof Hadith> {
 
   public routes() {
     this.router.get('/', this.bindHandler(Hadith.index))
+    this.router.get('/search', this.bindHandler(Hadith.searchByKeyword))
     this.router.get('/:name', this.bindHandler(Hadith.getByName))
+    this.router.get('/:name/search', this.bindHandler(Hadith.searchByKeyword))
     this.router.get('/:name/:number', this.bindHandler(Hadith.getByNumber))
   }
 }
